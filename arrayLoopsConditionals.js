46d2c9d82fb3d126bd25922c157adf8d33715143
Mark Soro <napoleon_soro@rocketmail.com>
2023-07-27 10:05:33 -0400

    get even answered

--- arrayLoopsConditionals.js
+++ arrayLoopsConditionals.js
@@ -5,6 +5,10 @@
  */
 
 console.log("\n======== PART II: JAVASCRIPT REPS ========\n");
-for (let i = 1; i <= 20; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 200; i += 2) {
+//   console.log(i);
+// }
