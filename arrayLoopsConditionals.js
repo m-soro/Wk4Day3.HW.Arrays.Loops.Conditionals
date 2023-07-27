6defe36dd877524b8c97fc8498c387430034fa01
Mark Soro <napoleon_soro@rocketmail.com>
2023-07-27 09:55:41 -0400

    easy going answered

---
+++ arrayLoopsConditionals.js
@@ -0,0 +1,10 @@
+/**
+ * Mark Soro
+ * WK4 DAY 3 LAB Assignment/Lab
+ * @link
+ */
+
+console.log("\n======== PART II: JAVASCRIPT REPS ========\n");
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
