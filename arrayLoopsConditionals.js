f8277cfaae515cc7657b2c074b1552ab5f949537
Mark Soro <napoleon_soro@rocketmail.com>
2023-07-27 10:30:03 -0400

    Wild Wild Life answered

--- arrayLoopsConditionals.js
+++ arrayLoopsConditionals.js
@@ -15,12 +15,31 @@ console.log("\n======== GET EVEN ========\n");
 // }
 console.log("\n======== FIZZ BUZZ ========\n");
 
-for (let i = 1; i <= 200; i++) {
-  i % 3 === 0 && i % 5 == 0
-    ? console.log(`FizzBuzz`)
-    : i % 3 === 0
-    ? console.log(`Fizz`)
-    : i % 5 === 0
-    ? console.log(`Buzz`)
-    : console.log(i);
-}
+// for (let i = 1; i <= 200; i++) {
+//   i % 3 === 0 && i % 5 == 0
+//     ? console.log(`FizzBuzz`)
+//     : i % 3 === 0
+//     ? console.log(`Fizz`)
+//     : i % 5 === 0
+//     ? console.log(`Buzz`)
+//     : console.log(i);
+// }
+
+console.log("\n======== WILD WILD LIFE ========\n");
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+
+plantee[2] = plantee[2] += 1;
+console.log(plantee[2]);
+wolfy[3] = "Gotham City";
+console.log(plantee[2]);
+dart.push("Hawkins");
+console.log(dart);
+// trying out the splice method
+wolfy.splice(0, 1);
+wolfy.splice(0, 0, "Gameboy");
+console.log(wolfy);
