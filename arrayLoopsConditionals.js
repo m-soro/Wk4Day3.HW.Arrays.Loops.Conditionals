72cebfffa5a0fc8e44a29337c7a8f03796c56def
Mark Soro <napoleon_soro@rocketmail.com>
2023-07-27 10:15:17 -0400

    fizz buzz answered

--- arrayLoopsConditionals.js
+++ arrayLoopsConditionals.js
@@ -5,10 +5,22 @@
  */
 
 console.log("\n======== PART II: JAVASCRIPT REPS ========\n");
+console.log("\n======== EASY GOING ========\n");
 // for (let i = 1; i <= 20; i++) {
 //   console.log(i);
 // }
-
+console.log("\n======== GET EVEN ========\n");
 // for (let i = 0; i <= 200; i += 2) {
 //   console.log(i);
 // }
+console.log("\n======== FIZZ BUZZ ========\n");
+
+for (let i = 1; i <= 200; i++) {
+  i % 3 === 0 && i % 5 == 0
+    ? console.log(`FizzBuzz`)
+    : i % 3 === 0
+    ? console.log(`Fizz`)
+    : i % 5 === 0
+    ? console.log(`Buzz`)
+    : console.log(i);
+}
